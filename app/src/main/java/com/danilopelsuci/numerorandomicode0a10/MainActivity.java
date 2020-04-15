c473356b484b5bfac3529d0ea9365407f4e7797e
danilo <danilopelusci@gmail.com>
2020-04-15 16:50:26 -0300

    primeiro projeto android

---
+++ app/src/main/java/com/danilopelsuci/numerorandomicode0a10/MainActivity.java
@@ -0,0 +1,25 @@
+package com.danilopelsuci.numerorandomicode0a10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void sortearNumero(View view) {
+        TextView texto = findViewById(R.id.textResult);
+        int x = new Random().nextInt(11);
+        texto.setText("numero: "+x);
+    }
+}
